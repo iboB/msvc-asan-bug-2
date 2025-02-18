4e7836c9ae8228c4d27a8971d51bd1c34af842a1
Borislav Stanimirov <b@ibob.bg>
2025-02-18 13:36:25 +0200

    initial commit

---
+++ no-deduce-no-repro.cpp
@@ -0,0 +1,14 @@
+#include <memory>
+#include <type_traits>
+template <typename stream>
+struct io {
+    io(stream) : b(false) {}
+    alignas(64) bool b;
+};
+int main() {
+    auto pi = std::make_unique<int>(543);
+    using iio = io<int>;
+    //io x(7);
+    iio x(7);
+    static_assert(std::is_same_v<decltype(x), iio>);
+}
---
+++ no-heap-alloc-no-repro.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+#include <type_traits>
+template <typename stream>
+struct io {
+    io(stream) : b(false) {}
+    alignas(64) bool b;
+};
+int main() {
+    //auto pi = std::make_unique<int>(543);
+    using iio = io<int>;
+    io x(7);
+    static_assert(std::is_same_v<decltype(x), iio>);
+}
